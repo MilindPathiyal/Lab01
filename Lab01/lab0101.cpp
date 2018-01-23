2997361d9078684aa07801f57d3ffb9a98915f32
Milind Paliath-Pathiyal <milindpathiyal@gmail.com>
2018-01-23 13:20:37 -0500

    Add files via upload

---
+++ Lab01/lab0101.cpp
@@ -0,0 +1,33 @@
+/*
+* Student Name: Milind Paliath-Pathiyal
+* Student Number: 20714398
+* SYDE 121 Lab: #1 Assignment: 001
+* Filename: Lab0101
+* Date submitted: 09/12/17
+*
+* I hereby declare that this code, submitted
+* for credit for the course SYDE121, is a product
+* of my own efforts. This coded solution has
+* not been plagiarized from other sources and
+* as not been knowingly plagiarized by others.
+*/
+//Lab0101 uses for loop to print out table of squares
+
+#include <iostream>
+using namespace std;
+int main( )
+{
+ int i = 0;
+ for ( i = 3; i < 10; i++ )
+ {
+ cout << "i = " << i
+ << " squared = "
+ << i * i
+ << endl;
+ }
+ cout << "\nA dope program by: Milindian\n";
+
+ system("pause");
+ return 0;
+}
+
